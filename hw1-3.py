d657f24d9ccff999ccd890a2f0e83961b12137a7
SubrataBauri <subrata@digitalaptech.com>
2018-03-27 06:31:15 +0530

    HW1-3

---
+++ hw1-3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pymongo
+import bottle
+import sys
+
+
+# Copyright 2014, MongoDB, Inc.
+# Author: Andrew Erlichson
+
+
+@bottle.get("/hw1/<n>")
+def get_hw1(n):
+
+    # connect to the db on standard port
+    connection = pymongo.MongoClient("mongodb://localhost")
+
+    n = int(n)
+
+    db = connection.m101                 # attach to db
+    collection = db.funnynumbers         # specify the collection
+
+    try:
+        iterator = collection.find({},limit=1, skip=n).sort('value', direction=1)
+        for item in iterator:
+            return str(int(item['value'])) + "\n"
+    except Exception as e:
+        print("Error trying to read collection:", type(e), e)
+
+
+bottle.debug(True)
+bottle.run(host='localhost', port=8080)
